a44ca8ccf526c73da0367f9f85425960373156f7
Jan Müller <janmueller3698@gmail.com>
2021-03-15 13:28:18 +0100

    feat: use masonry layout for blog and projects

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ export default {
   css: ['~/assets/style.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [{ src: '~/plugins/vue-masonry-wall' }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
