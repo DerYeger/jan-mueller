784f663c3f2558703e453e3636bfab494eae188e
Jan Müller <janmueller3698@gmail.com>
2021-03-16 11:34:56 +0100

    feat: update primary color for both themes

--- theme.config.js
+++ theme.config.js
@@ -1,13 +1,18 @@
 import colors from 'vuetify/es5/util/colors'
 
+const primaryColor = '#007aff'
+
 export const themes = {
   dark: {
-    primary: colors.blue.darken2,
+    primary: primaryColor,
     accent: colors.grey.darken3,
     secondary: colors.amber.darken3,
     info: colors.teal.lighten1,
     warning: colors.amber.base,
     error: colors.deepOrange.accent4,
     success: colors.green.accent3,
+  },
+  light: {
+    primary: primaryColor,
   },
 }
