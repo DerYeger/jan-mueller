91bc6625c3d73c545e8e677e03252c55d08250ed
Jan Müller <janmueller3698@gmail.com>
2021-03-16 16:14:09 +0100

    feat: add profile image to home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,33 @@
 <template>
-  <nuxt-content :document="document"></nuxt-content>
+  <v-col>
+    <v-row class="flex-wrap-reverse">
+      <v-col :sm="9" :xl="10">
+        <nuxt-content :document="document"></nuxt-content>
+      </v-col>
+      <v-col :sm="3" :xl="2">
+        <v-row justify="center" class="mb-4">
+          <v-avatar size="100%" justify="center">
+            <img
+              :src="require('~/assets/images/profile-image.jpg')"
+              alt="Jan"
+            />
+          </v-avatar>
+        </v-row>
+        <v-row justify="center" align-items="center">
+          <a
+            href="https://github.com/DerYeger"
+            class="account-link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <v-icon x-large v-text="'fab fa-github'" />
+            <span>GitHub</span>
+            <v-icon small v-text="'mdi-open-in-new'" />
+          </a>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-col>
 </template>
 
 <script lang="ts">
@@ -27,3 +55,15 @@ export default defineComponent({
   },
 })
 </script>
+
+<style lang="scss">
+.account-link {
+  text-decoration: none;
+  color: unset !important;
+
+  span {
+    margin-left: 0.5rem;
+    margin-right: 0.5rem;
+  }
+}
+</style>
