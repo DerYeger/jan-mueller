f5636bea99369638b52b5d8e2d4c35368cda9117
Jan Müller <janmueller3698@gmail.com>
2021-03-16 14:16:49 +0100

    test: provide vuetify to spec

--- test/LanguageToggle.spec.js
+++ test/LanguageToggle.spec.js
@@ -1,9 +1,17 @@
 import { mount, RouterLinkStub } from '@vue/test-utils'
 import LanguageToggle from '@/components/LanguageToggle.vue'
+import Vuetify from 'vuetify'
 
 describe('LanguageToggle', () => {
+  let vuetify
+
+  beforeEach(() => {
+    vuetify = new Vuetify()
+  })
+
   test('is a Vue instance', () => {
     const wrapper = mount(LanguageToggle, {
+      vuetify,
       mocks: {
         switchLocalePath: (i) => i,
       },
