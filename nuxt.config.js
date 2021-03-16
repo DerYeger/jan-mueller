dbde8dd041a58518bea48825c12eac4b8200e745
Jan Müller <janmueller3698@gmail.com>
2021-03-16 16:14:09 +0100

    feat: add profile image to home page

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,13 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://use.fontawesome.com/releases/v5.0.13/css/all.css',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
