617fac6b533398c68779660b210e6d1eb29fff1c
Jan Müller <janmueller3698@gmail.com>
2021-03-17 09:23:38 +0100

    feat: add hover effect to blog post card

--- components/BlogPostCard.vue
+++ components/BlogPostCard.vue
@@ -1,26 +1,32 @@
 <template>
-  <v-card :to="blogPost.path">
-    <v-img
-      v-if="blogPost.image"
-      :src="require('~/assets/images/' + blogPost.image)"
-      :aspect-ratio="2"
-      max-height="8rem"
+  <v-hover v-slot="{ hover }">
+    <v-card
+      :to="blogPost.path"
+      :elevation="hover ? 12 : 2"
+      :class="hover ? 'primary--text' : 'text'"
     >
-      <template #placeholder>
-        <v-row class="fill-height ma-0" align="center" justify="center">
-          <v-progress-circular indeterminate color="primary" />
-        </v-row>
+      <v-img
+        v-if="blogPost.image"
+        :src="require('~/assets/images/' + blogPost.image)"
+        :aspect-ratio="2"
+        max-height="8rem"
+      >
+        <template #placeholder>
+          <v-row class="fill-height ma-0" align="center" justify="center">
+            <v-progress-circular indeterminate color="primary" />
+          </v-row>
+        </template>
+      </v-img>
+      <v-card-title>{{ blogPost.title }}</v-card-title>
+      <v-card-subtitle>{{ formatDate(blogPost.createdAt) }}</v-card-subtitle>
+      <template v-if="hasTags(blogPost)">
+        <v-divider class="mx-4" />
+        <v-card-text>
+          {{ $t('misc.tags') }}: {{ blogPost.tags.join(', ') }}
+        </v-card-text>
       </template>
-    </v-img>
-    <v-card-title>{{ blogPost.title }}</v-card-title>
-    <v-card-subtitle>{{ formatDate(blogPost.createdAt) }}</v-card-subtitle>
-    <template v-if="hasTags(blogPost)">
-      <v-divider class="mx-4" />
-      <v-card-text>
-        {{ $t('misc.tags') }}: {{ blogPost.tags.join(', ') }}
-      </v-card-text>
-    </template>
-  </v-card>
+    </v-card>
+  </v-hover>
 </template>
 
 <script lang="ts">
