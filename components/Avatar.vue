01df8e2a7e72e5603562d9614ea9de9fd0e566ce
Jan Müller <janmueller3698@gmail.com>
2021-03-17 10:01:43 +0100

    refactor: enable lazy loading for avatar

---
+++ components/Avatar.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-avatar size="100%">
+    <v-img
+      :src="require('~/assets/images/avatar.webp')"
+      :lazy-src="require('~/assets/images/avatar-lazy.webp')"
+      alt="Jan"
+    >
+    </v-img>
+  </v-avatar>
+</template>
