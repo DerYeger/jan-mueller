1a1599b388860807392a41240ad86f23d925de36
Jan Müller <janmueller3698@gmail.com>
2021-04-03 20:47:03 +0200

    fix: update document switcher style

--- components/DocumentSwitcher.vue
+++ components/DocumentSwitcher.vue
@@ -1,11 +1,11 @@
 <template>
   <p class="link-container">
-    <NuxtLink v-if="prev" :to="prev.path">
+    <NuxtLink v-if="prev" :to="prev.path" class="mr-4">
       <v-icon v-text="'mdi-chevron-left'" />
       <span>{{ prev.title }}</span>
     </NuxtLink>
     <span v-else />
-    <NuxtLink v-if="next" :to="next.path">
+    <NuxtLink v-if="next" :to="next.path" style="text-align: right">
       <span>{{ next.title }}</span>
       <v-icon v-text="'mdi-chevron-right'" />
     </NuxtLink>
@@ -40,7 +40,7 @@ export default defineComponent({
     display: flex;
     align-items: center;
     color: unset !important;
-    font-size: 1.25rem;
+    font-size: clamp(1rem, 2vw, 1.25rem);
     font-weight: bold;
     text-decoration: none;
 
