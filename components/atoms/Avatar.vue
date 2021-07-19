6469d1198646064b18f7e2b4b3eb767cb7cf5b62
Jan Müller <janmueller3698@gmail.com>
2021-07-19 18:11:07 +0200

    fix: prevent fullscreen of avatar

--- components/atoms/Avatar.vue
+++ components/atoms/Avatar.vue
@@ -1,6 +1,11 @@
 <template>
   <v-avatar size="100%" width="100%" class="avatar elevation-6">
-    <asset-image src="avatar.webp" lazy-src="avatar-lazy.webp" alt="Jan" />
+    <asset-image
+      src="avatar.webp"
+      lazy-src="avatar-lazy.webp"
+      alt="Jan"
+      :allow-fullscreen="false"
+    />
   </v-avatar>
 </template>
 
