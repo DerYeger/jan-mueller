f9a52414dd7865cc58725fecd87e69628ac27351
Jan Müller <janmueller3698@gmail.com>
2022-01-16 21:39:36 +0100

    feat(home): update layout

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -8,23 +8,23 @@ const { t } = useI18n()
     <div class="flex-center flex-col sm-flex-row sm-gap-8">
       <Avatar class="pa-4" />
       <div>
-        <Facts />
-        <p class="prose" v-html="t('about.intro')" />
+        <Facts class="sm-text-right" />
+        <p class="prose sm-text-right" v-html="t('about.intro')" />
       </div>
     </div>
     <div
       class="flex-center flex-col sm-flex-row-reverse sm-space-between sm-gap-8"
     >
-      <Accounts />
-      <p class="prose" v-html="t('about.hobbies')" />
+      <Accounts class="sm-text-right" />
+      <p class="prose sm-text-left" v-html="t('about.hobbies')" />
     </div>
     <div class="flex-center flex-col sm-flex-row sm-gap-8">
-      <Languages />
-      <p class="prose" v-html="t('about.skills')" />
+      <Languages class="sm-text-left" />
+      <p class="prose sm-text-right" v-html="t('about.skills')" />
     </div>
     <div class="flex-center flex-col sm-flex-row-reverse sm-gap-8">
-      <Software />
-      <p class="prose" v-html="t('about.inspiration')" />
+      <Software class="sm-text-right" />
+      <p class="prose sm-text-left" v-html="t('about.inspiration')" />
     </div>
     <!-- eslint-enable vue/no-v-html -->
   </section>
