7e10fdd43ca2adc3112cbc9a789d6405a5d21634
Jan Müller <janmueller3698@gmail.com>
2022-01-16 16:20:36 +0100

    feat(home): add accounts and languages

---
+++ src/components/Languages.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const { t } = useI18n()
+</script>
+
+<template>
+  <aside class="text-center py-4">
+    <span class="text-xl">{{ t('languages') }} </span>
+    <div class="text-2xl children:mx-1">
+      <div class="i-logos-kotlin" />
+      <div class="i-logos-java" />
+      <div class="i-logos-haskell-icon" />
+      <div class="i-logos-python" />
+      <div class="i-logos-typescript-icon" />
+      <div class="i-logos-javascript" />
+      <div class="i-logos-html-5" />
+      <div class="i-logos-css-3" />
+      <div class="i-logos-sass" />
+    </div>
+  </aside>
+</template>
