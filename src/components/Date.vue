9c4376ef38c1c015a2a49ae61cd79c5cb6636074
Jan Müller <janmueller3698@gmail.com>
2022-01-18 09:21:21 +0100

    fix(date): make `format` prop optional

--- src/components/Date.vue
+++ src/components/Date.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
-const props = withDefaults(defineProps<{ date: string; format: string }>(), {
+const props = withDefaults(defineProps<{ date: string; format?: string }>(), {
   format: 'month',
 })
 
 const { date, format } = toRefs(props)
 
 const { d } = useI18n()
+
+const formattedDate = computed(() => d(new Date(date.value), format.value))
 </script>
 
 <template>
-  <span class="text-lighter">{{ d(new Date(date), format) }}</span>
+  <span class="text-lighter">{{ formattedDate }}</span>
 </template>
