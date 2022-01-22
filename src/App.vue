0105a591761822f50c0afbe0c10f578da017dac9
Jan Müller <janmueller3698@gmail.com>
2022-01-22 16:20:06 +0100

    feat(seo): add meta data

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,72 @@ const { t } = useI18n()
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
-useHead({
-  title: 'Jan Müller',
-  meta: [
-    {
-      name: 'description',
-      content: t('meta.description'),
-    },
-  ],
+const router = useRouter()
+
+const head = computed(() => {
+  const host = 'https://jan-mueller.at'
+  const title = 'Jan Müller'
+  const description = t('meta.description')
+  const ogImage = `${host}/og-logo.png`
+
+  return {
+    title,
+    link: [
+      {
+        rel: 'canonical',
+        href: `${host}${router.currentRoute.value.fullPath}`,
+      },
+    ],
+    meta: [
+      {
+        name: 'description',
+        content: description,
+      },
+      {
+        hid: 'twitter:title',
+        name: 'twitter:title',
+        content: title,
+      },
+      {
+        hid: 'twitter:description',
+        name: 'twitter:description',
+        content: description,
+      },
+      {
+        hid: 'twitter:image',
+        name: 'twitter:image',
+        content: ogImage,
+      },
+      {
+        hid: 'twitter:image:alt',
+        name: 'twitter:image:alt',
+        content: title,
+      },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: title,
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: description,
+      },
+      {
+        hid: 'og:image',
+        name: 'og:image',
+        content: ogImage,
+      },
+      {
+        hid: 'og:image:alt',
+        property: 'og:image:alt',
+        content: title,
+      },
+    ],
+  }
 })
+
+useHead(head)
 </script>
 
 <template>
