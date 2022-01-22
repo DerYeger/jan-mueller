8a37946218883ad09497aabe3976177c4aa44b12
Jan Müller <janmueller3698@gmail.com>
2022-01-22 16:09:18 +0100

    feat(files): add separate pages for documents and models

---
+++ src/pages/files/models/[name].vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { useModels } from '~/composables'
+
+const props = defineProps<{ name: string }>()
+
+const { name } = toRefs(props)
+
+const modelPath = computed(() => `/static/files/models/${name.value}.mview`)
+
+const router = useRouter()
+const models = useModels()
+watch(
+  name,
+  (name) => {
+    if (models.find((model) => model.name === name) === undefined) {
+      router.push('/files')
+    }
+  },
+  { immediate: true }
+)
+
+const { t } = useI18n()
+</script>
+
+<template>
+  <div class="flex flex-col h-full">
+    <h1 class="text-center">{{ t(`files.models.${name}.title`) }}</h1>
+    <p class="text-center text-lighter">
+      {{ t(`files.models.${name}.description`) }}
+    </p>
+    <div class="flex-1">
+      <MarmosetViewer :src="modelPath" responsive />
+    </div>
+  </div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: fullscreen
+</route>
