77a6700281bf1d464fed5335781dfa322b9d4b49
Jan Müller <janmueller3698@gmail.com>
2022-01-23 14:23:55 +0100

    fix: disable broken meta tags

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
-const { t } = useI18n()
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
-const router = useRouter()
+
+const { t } = useI18n()
+const description = t('meta.description')
 
 const host = 'https://jan-mueller.at'
-const description = t('meta.description')
-const ogImage = `${host}/og-logo.png`
+// const ogImage = `${host}/og-logo.png`
+
+const router = useRouter()
 
 useHead({
   link: [
@@ -21,26 +23,22 @@ useHead({
       name: 'description',
       content: description,
     },
-    {
-      hid: 'twitter:description',
-      name: 'twitter:description',
-      content: description,
-    },
-    {
-      hid: 'twitter:image',
-      name: 'twitter:image',
-      content: ogImage,
-    },
-    {
-      hid: 'og:description',
-      property: 'og:description',
-      content: description,
-    },
-    {
-      hid: 'og:image',
-      name: 'og:image',
-      content: ogImage,
-    },
+    // {
+    //   name: 'twitter:description',
+    //   content: description,
+    // },
+    // {
+    //   name: 'twitter:image',
+    //   content: ogImage,
+    // },
+    // {
+    //   property: 'og:description',
+    //   content: description,
+    // },
+    // {
+    //   name: 'og:image',
+    //   content: ogImage,
+    // },
   ],
 })
 </script>
--- src/composables/title.ts
+++ src/composables/title.ts
@@ -7,27 +7,27 @@ export function useTitleTemplate(title: string): void {
   const pageTitle: Ref<string> = computed(() => `${t(title)} · ${siteTitle}`)
   useHead({
     title: pageTitle,
-    meta: [
-      {
-        hid: 'twitter:title',
-        name: 'twitter:title',
-        content: pageTitle,
-      },
-      {
-        hid: 'twitter:image:alt',
-        name: 'twitter:image:alt',
-        content: pageTitle,
-      },
-      {
-        hid: 'og:title',
-        property: 'og:title',
-        content: pageTitle,
-      },
-      {
-        hid: 'og:image:alt',
-        property: 'og:image:alt',
-        content: pageTitle,
-      },
-    ],
+    // meta: [
+    //   {
+    //     hid: 'twitter:title',
+    //     name: 'twitter:title',
+    //     content: pageTitle,
+    //   },
+    //   {
+    //     hid: 'twitter:image:alt',
+    //     name: 'twitter:image:alt',
+    //     content: pageTitle,
+    //   },
+    //   {
+    //     hid: 'og:title',
+    //     property: 'og:title',
+    //     content: pageTitle,
+    //   },
+    //   {
+    //     hid: 'og:image:alt',
+    //     property: 'og:image:alt',
+    //     content: pageTitle,
+    //   },
+    // ],
   })
 }
