060d93ef7a3373a5792389c11679dbfea7c63b61
Jan Müller <janmueller3698@gmail.com>
2022-01-24 09:29:29 +0100

    test: add e2e tests

---
+++ cypress/integration/models.spec.ts
@@ -0,0 +1,20 @@
+describe('Models', () => {
+  before(() => {
+    cy.visit('/files/models/an-26')
+  })
+
+  it('shows the title', () => {
+    cy.contains('An-26').should('be.visible')
+  })
+
+  it('loads the model viewer', () => {
+    cy.get('.marmoset-viewer-host')
+      .should('be.visible')
+      .within(() => {
+        cy.get('input')
+          .click()
+          .get('font')
+          .within(() => cy.contains('Jan Mueller'))
+      })
+  })
+})
---
+++ cypress/integration/posts.spec.ts
@@ -0,0 +1,9 @@
+describe('Posts', () => {
+  before(() => {
+    cy.visit('/blog/vue-masonry-wall')
+  })
+
+  it('shows the blog post', () => {
+    cy.contains('Masonry layout').should('be.visible')
+  })
+})
