6092538b99b3043b9f56487d526de9580ddbc8b5
Jan Müller <janmueller3698@gmail.com>
2022-01-24 09:00:15 +0100

    test: add e2e tests

--- cypress/integration/blog.spec.ts
+++ cypress/integration/blog.spec.ts
@@ -1,10 +1,35 @@
 context('Blog', () => {
   beforeEach(() => {
+    cy.viewport('iphone-xr')
     cy.visit('/blog')
   })
 
   it('can navigate to a post', () => {
     cy.contains('Masonry layout').click()
     cy.location('pathname').should('eq', '/blog/vue-masonry-wall')
+  })
+
+  it('has a single column on mobile', () => {
+    cy.get('.masonry-wall')
+      .find('.masonry-column')
+      .its('length')
+      .should('eq', 1)
+  })
+
+  it('has multiple columns on desktop', () => {
+    cy.viewport('macbook-16')
+    cy.get('.masonry-wall')
+      .find('.masonry-column')
+      .its('length')
+      .should('eq', 2)
+  })
+
+  it('shows the details of a blog post', () => {
+    cy.contains('Masonry layout').parentsUntil('.card').as('card')
+    cy.get('@card').within(() => {
+      cy.contains('Aug 16, 2021').should('be.visible')
+      cy.contains('English').should('be.visible')
+      cy.contains('SSR support').should('be.visible')
+    })
   })
 })
--- cypress/integration/footer.spec.ts
+++ cypress/integration/footer.spec.ts
@@ -4,7 +4,24 @@ context('Footer', () => {
   })
 
   it('has a link to the about page', () => {
-    cy.contains('About / Impressum').click()
+    cy.get('footer').within(() => cy.contains('About / Impressum').click())
     cy.location('pathname').should('eq', '/about')
+  })
+
+  it('has a link to the source', () => {
+    cy.get('footer').within(() =>
+      cy
+        .contains('Source')
+        .should('be.visible')
+        .should('have.attr', 'href', 'https://github.com/DerYeger/jan-mueller')
+    )
+  })
+
+  it('has a copyright notice', () => {
+    cy.get('footer').within(() =>
+      cy
+        .contains(`2021 - ${new Date().getFullYear()} © Jan Müller`)
+        .should('be.visible')
+    )
   })
 })
