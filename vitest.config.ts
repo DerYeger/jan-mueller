509cf57d28ad1a166ef7592efabdf8f1e37231b1
Jan Müller <janmueller3698@gmail.com>
2022-04-04 08:40:09 +0200

    chore: extract vitest config

---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  // https://github.com/vitest-dev/vitest
+  test: {
+    include: ['test/**/*.test.ts'],
+    environment: 'jsdom',
+    deps: {
+      inline: ['@vue', '@vueuse', 'vue-demi'],
+    },
+  },
+})
