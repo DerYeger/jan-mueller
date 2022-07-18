fbf9689d8570364350602ed6477cd4602685b0e9
Jan Müller <janmueller3698@gmail.com>
2022-07-18 08:41:59 +0200

    feat: improve spacing

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,31 +7,30 @@ const { t } = useI18n()
 </script>
 
 <template>
-  <section class="flex-center flex-col">
+  <section class="flex-center flex-col gap-16 children:gap-16">
     <!-- eslint-disable vue/no-v-html -->
-    <div
-      class="flex-center flex-col md-flex-row md-gap-8 w-full justify-between"
-    >
+    <div class="flex-center flex-col gap-0 md-flex-row w-full justify-between">
       <Avatar class="pa-4" />
       <div>
         <Facts class="text-center md-text-right" />
-        <p class="prose text-center md-text-right" v-html="t('about.intro')" />
+        <p
+          class="prose text-center md-text-right mt-16"
+          v-html="t('about.intro')"
+        />
       </div>
     </div>
     <div
-      class="flex-center flex-col md-flex-row-reverse md-space-between md-gap-8 w-full justify-between"
+      class="flex-center flex-col md-flex-row-reverse md-space-between w-full justify-between"
     >
       <Languages class="md-text-right" />
       <p class="prose text-center md-text-left" v-html="t('about.hobbies')" />
     </div>
-    <div
-      class="flex-center flex-col md-flex-row md-gap-8 w-full justify-between"
-    >
+    <div class="flex-center flex-col md-flex-row w-full justify-between">
       <FrameworksAndLibraries class="md-text-left" />
       <p class="prose text-center md-text-right" v-html="t('about.skills')" />
     </div>
     <div
-      class="flex-center flex-col md-flex-row-reverse md-gap-8 w-full justify-between"
+      class="flex-center flex-col md-flex-row-reverse w-full justify-between"
     >
       <ToolsAndDatabases class="md-text-right" />
       <p
@@ -45,7 +44,7 @@ const { t } = useI18n()
     <Projects />
   </section>
   <section
-    class="mt-16 flex flex-col md-flex-row items-start gap-4 children:flex-1 children:w-full"
+    class="mt-16 flex flex-col gap-16 md-flex-row items-start md:gap-4 children:flex-1 children:w-full"
   >
     <Experience />
     <Education class="!md-transition-delay-0.2s" />
