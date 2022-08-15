a060d66842beae2bc2dc01aead7176249972a53e
Jan Müller <janmueller3698@gmail.com>
2022-08-15 15:03:57 +0200

    feat: add masonry wall example

---
+++ src/components/blog/examples/VueMasonryWall.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+import MasonryWall from '@yeger/vue-masonry-wall'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  components: {
+    MasonryWall,
+  },
+  data() {
+    return {
+      items: [50, 75, 75, 100, 50, 50, 75, 150, 125, 175, 50, 100, 125],
+    }
+  },
+})
+</script>
+
+<template>
+  <MasonryWall :items="items" :ssr-columns="1" :column-width="100" :gap="16">
+    <template #default="{ item, index }">
+      <div
+        :style="{ height: `${item}px` }"
+        class="surface border rounded flex items-center justify-center"
+      >
+        {{ index }}
+      </div>
+    </template>
+  </MasonryWall>
+</template>
