e7b0594e7d8590c526bc3777fdfb545fb4048bf1
Jan Müller <janmueller3698@gmail.com>
2022-08-15 16:14:54 +0200

    feat: add theme toggle

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'media',
+  darkMode: 'class',
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {},
