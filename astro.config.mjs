2c59ac09397c32f8920500bdd205768d212ab714
Jan Müller <janmueller3698@gmail.com>
2022-08-16 23:24:59 +0200

    feat: prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,28 @@
-import image from '@astrojs/image'
-import mdx from '@astrojs/mdx'
-import react from '@astrojs/react'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
-import vue from '@astrojs/vue'
-import robotsTxt from 'astro-robots-txt'
-import { defineConfig } from 'astro/config'
+import image from '@astrojs/image';
+import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import vue from '@astrojs/vue';
+import robotsTxt from 'astro-robots-txt';
+import { defineConfig } from 'astro/config';
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jan-mueller.at',
   server: {
-    host: true,
+    host: true
   },
-  integrations: [
-    image(),
-    mdx(),
-    sitemap(),
-    react(),
-    tailwind({ config: { applyBaseStyles: false } }),
-    vue(),
-    robotsTxt(),
-  ],
+  integrations: [image(), mdx(), sitemap(), react(), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), vue(), robotsTxt(), prefetch()],
   markdown: {
     shikiConfig: {
-      theme: 'monokai',
-    },
-  },
-})
+      theme: 'monokai'
+    }
+  }
+});
