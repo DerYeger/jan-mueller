4b61cf8133646e2cd92391ce08eba23a9aca9a2b
Jan Müller <janmueller3698@gmail.com>
2022-08-17 12:52:34 +0200

    chore: format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,36 @@
-import image from '@astrojs/image';
-import mdx from '@astrojs/mdx';
-import react from '@astrojs/react';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import vue from '@astrojs/vue';
-import robotsTxt from 'astro-robots-txt';
-import { defineConfig } from 'astro/config';
-
-import prefetch from "@astrojs/prefetch";
+import image from '@astrojs/image'
+import mdx from '@astrojs/mdx'
+import prefetch from '@astrojs/prefetch'
+import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import vue from '@astrojs/vue'
+import robotsTxt from 'astro-robots-txt'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jan-mueller.at',
   server: {
-    host: true
+    host: true,
   },
-  integrations: [image(), mdx(), sitemap(), react(), tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), vue(), robotsTxt(), prefetch()],
+  integrations: [
+    image(),
+    mdx(),
+    sitemap(),
+    react(),
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    vue(),
+    robotsTxt(),
+    prefetch(),
+  ],
   markdown: {
     shikiConfig: {
-      theme: 'monokai'
-    }
-  }
-});
+      theme: 'monokai',
+    },
+  },
+})
