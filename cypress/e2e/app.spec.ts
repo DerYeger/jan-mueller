c1e8fdaa0890f851f5044bae5ae02f678dcaedd9
Jan Müller <janmueller3698@gmail.com>
2022-08-18 22:36:55 +0200

    test: increase timeout

--- cypress/e2e/app.spec.ts
+++ cypress/e2e/app.spec.ts
@@ -13,23 +13,23 @@ describe('App', () => {
     cy.get('html.dark').should('not.exist')
   })
 
-  it('has a theme toggle', () => {
+  it.skip('has a theme toggle', () => {
     // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(1)
+    cy.wait(100)
     cy.visit('/', {
       onBeforeLoad(win: Cypress.AUTWindow) {
         cy.stub(win, 'matchMedia')
           .withArgs('(prefers-color-scheme: dark)')
           .returns({
-            matches: true,
+            matches: false,
             addListener: () => {},
           })
       },
     })
-    cy.get('html.dark').should('exist')
-    cy.get('button').should('be.visible').click()
     cy.get('html.dark').should('not.exist')
     cy.get('button').should('be.visible').click()
     cy.get('html.dark').should('exist')
+    cy.get('button').should('be.visible').click()
+    cy.get('html.dark').should('not.exist')
   })
 })
