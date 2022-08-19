c60f674c20589fafda1aa9e0bdd3a7d8b1bac956
Jan Müller <janmueller3698@gmail.com>
2022-08-19 15:28:09 +0200

    feat: configure rss

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,18 @@
+import rss from '@astrojs/rss'
+
+export const get = () =>
+  rss({
+    title: `Jan's Blog`,
+    // `<description>` field in output xml
+    description: `Jan's Blog`,
+    // base URL for RSS <item> links
+    // SITE will use "site" from your project's astro.config.
+    site: import.meta.env.SITE,
+    // list of `<item>`s in output xml
+    // simple example: generate items for every md file in /src/pages
+    // see "Generating items" section for required frontmatter and advanced use cases
+    items: import.meta.glob('./blog/**/*.mdx'),
+    // (optional) inject custom xml
+    customData: `<language>en-us</language>`,
+    stylesheet: '/rss/pretty-feed-v3.xsl',
+  })
