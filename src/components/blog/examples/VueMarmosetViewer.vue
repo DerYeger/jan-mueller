ef46a2934e984891b5d2c27ec33b6ae2fcf79b07
Jan Müller <janmueller3698@gmail.com>
2022-09-05 11:16:27 +0200

    feat: add blog post

---
+++ src/components/blog/examples/VueMarmosetViewer.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import * as MarmosetViewer from 'vue-marmoset-viewer'
+
+export default defineComponent({
+  components: {
+    MarmosetViewer: MarmosetViewer.MarmosetViewer,
+  },
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+    height: {
+      type: Number,
+      default: 300,
+    },
+  },
+})
+</script>
+
+<template>
+  <div class="shadow-md rounded overflow-hidden relative">
+    <MarmosetViewer :src="src" responsive :height="height" />
+  </div>
+</template>
