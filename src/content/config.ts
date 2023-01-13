b01ae127bc362ba1c1c0735c60b9d8cad620b796
Jan Müller <janmueller3698@gmail.com>
2023-01-13 17:36:29 +0100

    feat: use content collections

---
+++ src/content/config.ts
@@ -0,0 +1,24 @@
+// eslint-disable-next-line import/no-unresolved
+import { defineCollection, z } from 'astro:content'
+import moment from 'moment'
+
+const DateSchema = z.preprocess((arg) => {
+  if (typeof arg == 'string' || arg instanceof Date) {
+    return moment(arg).toDate()
+  }
+}, z.date())
+
+const blog = defineCollection({
+  slug: ({ id }) => {
+    return id.replace('.mdx', '')
+  },
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    heroImage: z.string(),
+    pubDate: DateSchema,
+    updatedDate: DateSchema.optional(),
+  }),
+})
+
+export const collections = { blog }
