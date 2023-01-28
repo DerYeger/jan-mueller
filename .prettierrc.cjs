f7a2d0c03d1ef90b39c25cb1f0946c5f20695a4f
Jan Müller <janmueller3698@gmail.com>
2023-01-28 09:23:06 +0100

    feat: only animate showcase if js is enabled

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,4 +2,12 @@ module.exports = {
   semi: false,
   singleQuote: true,
   plugins: [require('prettier-plugin-astro')],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
 }
