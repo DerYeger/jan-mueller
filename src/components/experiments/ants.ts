0818a66ef6bc7f8a0ff7178b56a3efeb4303147f
Jan Müller <janmueller3698@gmail.com>
2023-02-05 09:00:25 +0100

    feat: improve experiments

--- src/components/experiments/ants.ts
+++ src/components/experiments/ants.ts
@@ -1,5 +1,3 @@
-const amount = 5
-
 function randomInRange(min: number, max: number) {
   return Math.random() * (max - min) + min
 }
@@ -26,65 +24,62 @@ function letThemLoose(container: HTMLElement) {
     canvas.style.height = `${container.getBoundingClientRect().height}`
   }).observe(container)
 
-  const ants = Array.from({ length: amount }, (_, index) => {
-    return {
-      x: randomInRange(0, canvas.width),
-      y: canvas.height,
-      direction: randomDirection(),
-      id: index,
-    }
-  })
+  const ant = {
+    x: randomInRange(0, canvas.width),
+    y: canvas.height,
+    direction: randomDirection(),
+  }
 
-  ants.forEach(({ x, y, direction }) => {
-    // draw a black dot at the location given by x and y on the canvas
-    const ctx = canvas.getContext('2d')
-    if (!ctx) {
+  // draw a black dot at the location given by x and y on the canvas
+  const ctx = canvas.getContext('2d')
+  if (!ctx) {
+    return
+  }
+
+  const interval = window.setInterval(() => {
+    // direction is a tuple of two numbers, the first one is the x direction and the second one is the y direction. add a black dot relative to the coordinates x and y to the canvas
+    const [xDirection, yDirection] = ant.direction
+    // draw a line from the previous coordinates to the new coordinates
+    ctx.beginPath()
+    ctx.moveTo(ant.x, ant.y)
+    ctx.lineTo(ant.x + xDirection, ant.y + yDirection)
+    ctx.strokeStyle = '#a08679'
+    ctx.lineWidth = 2
+    ctx.stroke()
+
+    // with a 10% chance, change the direction
+    if (Math.random() < 0.5) {
+      ant.direction = randomDirection()
+    }
+
+    const { width, height } = container.getBoundingClientRect()
+
+    // if the x coordinate is bigger than the window width or smaller than 0, change the x direction
+    if (ant.x + xDirection > width || ant.x + xDirection < 0) {
+      ant.direction[0] = -xDirection
+    }
+    if (ant.y + yDirection <= 0) {
+      window.clearInterval(interval)
       return
     }
-
-    const _interval = window.setInterval(() => {
-      // direction is a tuple of two numbers, the first one is the x direction and the second one is the y direction. add a black dot relative to the coordinates x and y to the canvas
-      const [xDirection, yDirection] = direction
-      // draw a line from the previous coordinates to the new coordinates
-      ctx.beginPath()
-      ctx.moveTo(x, y)
-      ctx.lineTo(x + xDirection, y + yDirection)
-      ctx.strokeStyle = '#a08679'
-      ctx.lineWidth = 2
-      ctx.stroke()
-
-      // with a 10% chance, change the direction
-      if (Math.random() < 0.5) {
-        direction = randomDirection()
-      }
-
-      const { width, height } = container.getBoundingClientRect()
-
-      // if the x coordinate is bigger than the window width or smaller than 0, change the x direction
-      if (x + xDirection > width || x + xDirection < 0) {
-        direction[0] = -xDirection
-      }
-      // if the y coordinate is bigger than the window height or smaller than 0, change the y direction
-      if (y + yDirection > height || y + yDirection < 0) {
-        direction[1] = -yDirection
-        // window.clearInterval(interval)
-      }
-      // set the new coordinates
-      x += xDirection
-      y += yDirection
-    }, 100)
-  })
+    // if the y coordinate is bigger than the window height or smaller than 0, change the y direction
+    if (ant.y + yDirection > height) {
+      ant.direction[1] = -yDirection
+    }
+    // set the new coordinates
+    ant.x += xDirection
+    ant.y += yDirection
+  }, 100)
 }
 
 document
   .querySelectorAll<HTMLButtonElement>('button.ant-trigger')
   .forEach((button) => {
     button.addEventListener('click', () => {
-      const container = button.parentElement
+      const container = document.querySelector<HTMLElement>('.ant-container')
       if (!container) {
         return
       }
-      container.removeChild(button)
       letThemLoose(container)
     })
   })
