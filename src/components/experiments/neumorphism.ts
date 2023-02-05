f184ae499cf83004772017e253feeb341c943574
Jan Müller <janmueller3698@gmail.com>
2023-02-05 12:17:23 +0100

    feat(experiments): add toggle

---
+++ src/components/experiments/neumorphism.ts
@@ -0,0 +1,15 @@
+export {}
+
+document
+  .querySelectorAll<HTMLInputElement>('#neon-bar-toggle')
+  .forEach((toggle) => {
+    toggle.addEventListener('change', (event) => {
+      const target = event.target as HTMLInputElement
+      const bar = document.querySelector('.neon-bar') as HTMLElement
+      if (target.checked) {
+        bar.style.setProperty('--from', '#44bb44')
+      } else {
+        bar.style.setProperty('--from', 'red')
+      }
+    })
+  })
