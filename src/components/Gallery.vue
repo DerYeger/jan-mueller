b5b20b2585dd50101061922f896e640baa08d9d8
Jan Müller <janmueller3698@gmail.com>
2024-01-30 17:28:54 +0100

    feat: load photography image dynamically

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,43 +1,24 @@
 <script setup lang="ts">
 import MasonryWall from '@yeger/vue-masonry-wall'
 
-interface Image {
+const props = defineProps<{ images: GalleryImage[] }>()
+
+export interface GalleryImage {
   src: string
   alt: string
   aspectRatio: number
 }
-
-const images: Image[] = [
-  { src: '/static/photo/2E8A1753.jpg', alt: 'Monkey', aspectRatio: 4 / 5 },
-  {
-    src: '/static/photo/2E8A4153.jpg',
-    alt: 'Wall of green leaves',
-    aspectRatio: 3 / 2,
-  },
-  { src: '/static/photo/2E8A3901.jpg', alt: 'Bunny', aspectRatio: 4 / 5 },
-  {
-    src: '/static/photo/2E8A3793.jpg',
-    alt: 'Play of light beneath a bridge',
-    aspectRatio: 3 / 2,
-  },
-  { src: '/static/photo/2E8A3296.jpg', alt: 'Squirrel', aspectRatio: 4 / 5 },
-  { src: '/static/photo/2E8A3395.jpg', alt: 'Tit bird', aspectRatio: 4 / 5 },
-  { src: '/static/photo/2E8A3993.jpg', alt: 'Rabbit', aspectRatio: 3 / 2 },
-  { src: '/static/photo/2E8A1130.jpg', alt: 'Crow', aspectRatio: 4 / 5 },
-  { src: '/static/photo/2E8A1174.jpg', alt: 'Heron', aspectRatio: 3 / 2 },
-  { src: '/static/photo/2E8A1210.jpg', alt: 'Heron', aspectRatio: 4 / 5 },
-]
 </script>
 
 <template>
   <div class="bg-white p-4">
     <MasonryWall
       v-slot="{ item: image }"
-      :items="images"
+      :items="props.images"
       :ssr-columns="1"
       :column-width="384"
       :gap="16"
-      :key-mapper="(image: Image) => image.src"
+      :key-mapper="(image: GalleryImage) => image.src"
     >
       <div
         class="h-0 shadow-sm"
@@ -49,7 +30,7 @@ const images: Image[] = [
           loading="lazy"
           decoding="async"
           oncontextmenu="return false"
-          class="w-full"
+          class="w-full shadow-2xl"
         />
       </div>
     </MasonryWall>
